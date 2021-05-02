cd1c47232feb66d8dc6da248c678b4677722b925
Kivi K <windore.wi@gmail.com>
2021-05-02 10:12:43 +0300

    Added validity checks

--- src/Windore.Settings.GUI.TestApp/ExampleSettings.cs
+++ src/Windore.Settings.GUI.TestApp/ExampleSettings.cs
@@ -7,8 +7,10 @@ namespace Windore.Settings.GUI.TestApp
         [Setting("Your name", "Basic Information")]
         public string Name { get; set; } = "";
         [Setting("Your age", "Basic Information")]
+        [IntSettingValueLimits(0, 130)]
         public int Age { get; set; }
         [Setting("Your height", "Basic Information")]
+        [DoubleSettingValueLimits(0, 3)]
         public double Height { get; set; }
         [Setting("Do you agree?", "Other")]
         public bool Agree { get; set; }
